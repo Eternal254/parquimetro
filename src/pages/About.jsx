8de6e26959a5239fc950e46eaefadff3526df395
JuaMedCa <jm689580@gmail.com>
2024-03-28 19:12:18 -0600

    registro casi termonado

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import '../App.css'
 
 const About = () => {
-    return <div className="default">
+    return <div className="default1">
         <h1>¿Que es esto?</h1>
         <p>Esto es una practica sobre react</p>
         <img src="https://i.pinimg.com/originals/92/19/67/9219675b2435637d1437eac61750e35a.gif"></img>
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -4,8 +4,15 @@ import '../App.css'
 const Register = () => {
     return(
         <div className="default">
-            <h1>Registro</h1>
-            <h2>This is our register</h2>
+            <h1 className="nom" >Nombre</h1>
+            <input type="text" placeholder="Nombre" className="inp"/>
+            <h1 className="nom">Matricula</h1>
+            <input type="text" placeholder="Matricula" className="inp" />
+            <br />  
+            <br />
+            <button className="button1">Guardar</button>
+            <br />
+            <br />
         </div>
     );
 }
