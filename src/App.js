21abcf4704b60bfe24e3181fbdcea0592e071e3b
JuaMedCa <jm689580@gmail.com>
2024-03-29 20:32:29 -0600

    Espacios de estacionamiento es una idea

--- src/App.js
+++ src/App.js
@@ -4,17 +4,19 @@ import About from './pages/About';
 import Layout from './pages/Layout';
 import Register from './pages/Register';
 import Footer from './pages/Footer';
+import Cuadros from './pages/Cuadros';
 
 function App() {
   return (
     <div>
       <Routes>
         <Route path='/' element={<Layout />}>
-          <Route path='/' element={<Register/>}></Route>
+          <Route path='/' element={<Cuadros/>}></Route>
           <Route path='about' element={<About/>}></Route>
+          <Route path="Register" element={<Register/>}></Route>
+
         </Route>
       </Routes>
-      <Footer/>
     </div>
   );
 }
---
+++ src/pages/Cuadros.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import '../App.css'
+import Register from "./Register";
+
+const Cuadros = () => {
+    return(
+        <div class="Cuadros">
+    <div>
+        <button class="cuadro1" onClick={() => window.location.href = 'Register'}>Espacio 1</button>
+        <button class="cuadro2" onClick={() => window.location.href = 'Register'}>Espacio 2</button>
+        <button class="cuadro3" onClick={() => window.location.href = 'Register'}>Espacio 3</button>
+        <button class="cuadro4" onClick={() => window.location.href = 'Register'}>Espacio 4</button>
+    </div>
+    <div>
+        <button class="cuadro5" onClick={() => window.location.href = 'Register'}>Espacio 5</button>
+        <button class="cuadro6" onClick={() => window.location.href = 'Register'}>Espacio 6</button>
+        <button class="cuadro7" onClick={() => window.location.href = 'Register'}>Espacio 7</button>
+        <button class="cuadro8" onClick={() => window.location.href = 'Register'}>Espacio 8</button>
+    </div>
+    <div>
+        <button class="cuadro9" onClick={() => window.location.href = 'Register'}>Espacio 9</button>
+        <button class="cuadro10" onClick={() => window.location.href = 'Register'}>Espacio 10</button>
+        <button class="cuadro11" onClick={() => window.location.href = 'Register'}>Espacio 11</button>
+        <button class="cuadro12" onClick={() => window.location.href = 'Register'}>Espacio 12</button>
+    </div>
+    <div>
+        <button class="cuadro13" onClick={() => window.location.href = 'Register'}>Espacio 13</button>
+        <button class="cuadro14" onClick={() => window.location.href = 'Register'}>Espacio 14</button>
+        <button class="cuadro15" onClick={() => window.location.href = 'Register'}>Espacio 15</button>
+        <button class="cuadro16" onClick={() => window.location.href = 'Register'}>Espacio 16</button>
+    </div>
+    <div>
+        <button class="cuadro17" onClick={() => window.location.href = 'Register'}>Espacio 17</button>
+        <button class="cuadro18" onClick={() => window.location.href = 'Register'}>Espacio 18</button>
+        <button class="cuadro19" onClick={() => window.location.href = 'Register'}>Espacio 19</button>
+        <button class="cuadro20" onClick={() => window.location.href = 'Register'}>Espacio 20</button>
+    </div>
+    <div>
+        <button class="cuadro21" onClick={() => window.location.href = 'Register'}>Espacio 21</button>
+        <button class="cuadro22" onClick={() => window.location.href = 'Register'}>Espacio 22</button>
+        <button class="cuadro23" onClick={() => window.location.href = 'Register'}>Espacio 23</button>
+        <button class="cuadro24" onClick={() => window.location.href = 'Register'}>Espacio 24</button>
+    </div>
+    <div>
+        <button class="cuadro25" onClick={() => window.location.href = 'Register'}>Espacio 25</button>
+        <button class="cuadro26" onClick={() => window.location.href = 'Register'}>Espacio 26</button>
+        <button class="cuadro27" onClick={() => window.location.href = 'Register'}>Espacio 27</button>
+        <button class="cuadro28" onClick={() => window.location.href = 'Register'}>Espacio 28</button>
+    </div>
+</div>
+
+    
+
+    );
+}
+
+export default Cuadros;
